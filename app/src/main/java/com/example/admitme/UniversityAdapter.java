72451ba742a9024a0fa106480cffa6c2357f07f9
RD-Singh <rdmalhans.02@gmail.com>
2020-06-15 17:43:22 -0700

    Test

--- app/src/main/java/com/example/admitme/UniversityAdapter.java
+++ app/src/main/java/com/example/admitme/UniversityAdapter.java
@@ -1,5 +1,6 @@
 package com.example.admitme;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,11 +9,18 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
@@ -21,14 +29,15 @@ public class UniversityAdapter extends RecyclerView.Adapter<UniversityAdapter.Un
 
     private ArrayList<Universities> mUniversities;
     private FirebaseFirestore fStore;
+    private FirebaseAuth fAuth;
 
     public static class UniversityViewHolder extends RecyclerView.ViewHolder {
 
-        public static TextView mUniversityName;
-        public static TextView mLocation;
-        public static ImageView mBookmarkImage;
-        public static CardView mItemView;
-        public static boolean counter;
+        public TextView mUniversityName;
+        public TextView mLocation;
+        public ImageView mBookmarkImage;
+        public CardView mItemView;
+        public boolean counter;
 
         public UniversityViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -54,14 +63,16 @@ public class UniversityAdapter extends RecyclerView.Adapter<UniversityAdapter.Un
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.university_item, parent, false);
 
         UniversityViewHolder universityViewHolder = new UniversityViewHolder(v);
-        return universityViewHolder;
 
+        return universityViewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull final UniversityViewHolder holder, int position) {
 
         final Universities currentItem = mUniversities.get(position);
+
+        fAuth = FirebaseAuth.getInstance();
 
         holder.mUniversityName.setText(currentItem.getUniversityName());
         holder.mLocation.setText(currentItem.getUniversityLocation());
@@ -70,7 +81,27 @@ public class UniversityAdapter extends RecyclerView.Adapter<UniversityAdapter.Un
         holder.mBookmarkImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                DocumentReference dRef = fStore.getInstance().collection("UNIVERSITIES").document(currentItem.getUniversityName());
+                DocumentReference dRef = fStore.getInstance().collection("ACCOUNTS").document(fAuth.getCurrentUser().getUid())
+                        .collection("SELECTED UNIVERSITIES").document(currentItem.getUniversityName());
+
+                dRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+
+                        if(task.isSuccessful())
+                        {
+                            DocumentSnapshot documentSnapshot = task.getResult();
+                            if(documentSnapshot.exists())
+                            {
+
+                            }else{
+
+                            }
+                        }
+
+                    }
+                });
+
 
                 if (holder.counter) {
 
